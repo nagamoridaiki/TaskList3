1e04bc29988f9b7ddf5d839efeed59767488833e
nagamoridaiki <no.swea1017@gmail.com>
2020-12-31 02:22:34 +0900

    課題③-2 修正

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ const button = document.getElementById("button")
 const taskList = document.getElementById("taskList")
 let progressList = document.getElementsByName('process')
 let newTask = document.getElementById("newTask")
-let inicialTableTag = 
+let initialTableTag = 
 '<tr>' +
     '<th>' + "ID" + '</th>' +
     '<th>' + "コメント" + '</th>' +
     '<th>' + "状態" + '</th>' +
 '<tr>'
-taskList.innerHTML = inicialTableTag
+taskList.innerHTML = initialTableTag
 const tasks = [];
 
 //新規タスクの追加ボタンが押下された時の処理
 button.addEventListener("click", ()=>{
-    if(newTask.value == ""){
+    if(newTask.value === ""){
         alert("入力が空です。")
     }else{
         //selectedProgress = checkProgress()
         //tasks.push({ taskName: newTask.value, progress: selectedProgress });
         tasks.push({ taskName: newTask.value, progress: "" });
-        newTask.value == ""
+        newTask.value = ""
         display()
     }
 })
@@ -38,7 +38,7 @@ function checkProgress(){
 
 //追加されたタスクの画面描画
 function display(){
-    taskList.innerHTML = inicialTableTag
+    taskList.innerHTML = initialTableTag
 
     for (let i = 0 ; i < tasks.length ; i++) {
         const tr = document.createElement("tr")
@@ -53,7 +53,7 @@ function display(){
         taskList.appendChild(tr)
 
         removeBtn = document.getElementsByClassName('remove')
-        for (var k=0; k < removeBtn.length; k++) {
+        for (let k=0; k < removeBtn.length; k++) {
             removeBtn[k].addEventListener('click', removeItem);
         };
     }
@@ -61,7 +61,7 @@ function display(){
 
 // 削除ボタンが押されたら発動
 function removeItem(){
-    var id = this.getAttribute('id');
+    const id = this.getAttribute('id');
     tasks.splice(id, 1);
     display()
 }
