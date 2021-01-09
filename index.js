4ffa0fad8c20d89b7468ebad095152e6b55523c9
nagamoridaiki <no.swea1017@gmail.com>
2021-01-09 19:43:36 +0900

    課題③-4 タスク表示/非表示切り替え機能

--- index.js
+++ index.js
@@ -3,75 +3,109 @@ const taskList = document.getElementById('taskList')
 let progressList = document.getElementsByName('process')
 let newTask = document.getElementById('newTask')
 let initialTableTag = 
-'<tr>' +
     '<th>' + 'ID' + '</th>' +
     '<th>' + 'コメント' + '</th>' +
-    '<th>' + '状態' + '</th>' +
-'<tr>'
-taskList.innerHTML = initialTableTag
+    '<th>' + '状態' + '</th>'
+taskList.innerHTML = initialTableTag;
 const tasks = [];
+let displayTasks = [];
 const progressStatus = {
     running : '作業中',
     complete : '完了'
 }
+let indexcount = 1;
 
 //新規タスクの追加ボタンが押下された時の処理
 button.addEventListener('click', ()=>{
     if(newTask.value === ''){
-        alert('入力が空です。')
+        alert('入力が空です。');
     }else{
-        tasks.push({ taskName : newTask.value, progress : progressStatus.running });
-        newTask.value = ''
-        display()
+        tasks.push({
+            index : indexcount,
+            taskName : newTask.value,
+            progress : progressStatus.running
+        });
+        newTask.value = '';
+        indexcount += 1;
+        display();
     }
 })
 
-
 //追加されたタスクの画面描画
 function display() {
-    taskList.innerHTML = initialTableTag
+    taskList.innerHTML = initialTableTag;
+    displayTasks = switchMode();
 
-    for (let i = 0 ; i < tasks.length ; i++) {
-        const tr = document.createElement("tr")
+    for (let i = 0 ; i < displayTasks.length ; i++) {
+        const tr = document.createElement("tr");
+        tr.setAttribute('id', i);
         tr.innerHTML = 
-        '<td>' + i + '</td>' + 
-        '<td>' + tasks[i].taskName + '</td>' + 
+        '<td>' + displayTasks[i].index + '</td>' + 
+        '<td>' + displayTasks[i].taskName + '</td>' + 
         '<td>' + 
-            '<button class=next>' + tasks[i].progress + '</button>' +
+            '<button class=next>' + displayTasks[i].progress + '</button>' +
             '<button class=remove>' + "削除" + '</button>' +
         '</td>'
-        taskList.appendChild(tr)
+        taskList.appendChild(tr);
 
-        removeBtn = document.getElementsByClassName('remove')
+        removeBtn = document.getElementsByClassName('remove');
         for (let k = 0 ; k < removeBtn.length ; k++) {
-            removeBtn[k].setAttribute('id', k)
+            removeBtn[k].setAttribute('id', k);
             removeBtn[k].addEventListener('click', removeItem);
         };
 
-        nextbtn = document.getElementsByClassName('next')
+        nextbtn = document.getElementsByClassName('next');
         for (let n = 0 ; n < nextbtn.length ; n++) {
-            nextbtn[n].setAttribute('id', n)
+            nextbtn[n].setAttribute('id', n);
             nextbtn[n].addEventListener('click', moveOnProgress);
         };
     }
 }
 
-// 削除ボタンが押されたら発動
+// 作業項目の削除
 function removeItem() {
-    
     const id = this.getAttribute('id');
     tasks.splice(id, 1);
-    display()
+    attachIndex();//再度tesksの番号を割り振る
+    display();
 }
 
-//3-3
-//作業中から完了
-//完了から作業中へ
+//作業進捗の切り替え
 function moveOnProgress() {
     const id = this.getAttribute('id');
-    tasks[id].progress === '作業中' ? tasks[id].progress = '完了' : tasks[id].progress = '作業中'
-    display()
+    if (displayTasks[id].progress === progressStatus.running) {//現在の進捗が進行中ならば切り替え
+        displayTasks[id].progress = progressStatus.complete;//完了
+    }else{
+        displayTasks[id].progress = progressStatus.running;//進行中
+    }
+    display();
 }
 
+//作業進捗の選択によって表示非表示の切り替え
+function switchMode() {
+    if(tasks){
+        displayTasks = [];
+        if (progressList[1].checked) {//進行中にチェックがついていた場合
+            for (let n = 0 ; n < tasks.length ; n++) {
+                tasks[n].progress === progressStatus.running ? displayTasks.push(tasks[n]) : null;
+            }
+        }else if (progressList[2].checked) {
+            for (let n = 0 ; n < tasks.length ; n++) {
+                tasks[n].progress === progressStatus.complete ? displayTasks.push(tasks[n]) : null;
+            }
+        }else{
+            for (let n = 0 ; n < tasks.length ; n++) {
+                displayTasks.push(tasks[n]);
+            }
+        }
+    }
+    return displayTasks;
+}
 
-
+//再度tesksの番号を割り振る
+function attachIndex() {
+    for(let n = 0 ; n < tasks.length ; n++) {
+        tasks[n].index = n;
+    }
+    indexcount = tasks.length;
+}
